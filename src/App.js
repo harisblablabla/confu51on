48a16a2b8cffe44d42100f272272dbad3901022b
haris fadhilah <haris.fadhilah21@gmail.com>
2021-02-15 02:38:03 +0700

    redux 1 (reducer,create store)

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import './App.css';
 import React, { Component } from 'react';
 import Main from './components/MainComponent'
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from './redux/configureStore';
 
+const store = configureStore()
 
 class App extends Component {
 
   render(){
     return(
-      <BrowserRouter>
-        <div className="App">
-          <Main/>
-        </div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main/>
+          </div>
       </BrowserRouter> 
+      </Provider>
     )
   }
 
---
+++ src/redux/configureStore.js
@@ -0,0 +1,10 @@
+import { createStore } from "redux"
+import {Reducer, initialState } from './reducer'
+
+export const configureStore = () => {
+    const store = createStore(
+        Reducer, //reducer
+        initialState ) //initial state
+
+    return store
+}
