48538514fdf14b96f9beb0917d201fc5259bd083
haris fadhilah <haris.fadhilah21@gmail.com>
2021-02-18 19:14:18 +0700

    assignment 3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,8 +1,105 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Control, Errors, LocalForm } from 'react-redux-form';
 import { Link } from 'react-router-dom';
-import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardImg, CardText, CardTitle } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, Button, Card, CardBody, CardImg, CardText, CardTitle, FormGroup, Label, Modal, ModalBody, ModalHeader } from 'reactstrap';
 
+    // ====validation====== //
 
+    const minLength = (len) => (val) => val && (val.length >= len);
+    const maxLength = (len) => (val) => !val || (val.length <= len);
+
+    // ====validation====== //
+
+class CommentForm extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            isModalOpen: false
+        }
+        this.toogleModal = this.toogleModal.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    toogleModal(){
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        })
+    }
+
+    handleSubmit(values) {
+        console.log(values)
+        alert('current value : ' + JSON.stringify(values))
+    }
+    
+    render() {
+        return(
+            <div>
+                <Button outline onClick={this.toogleModal} className="secondary"><span className="fa fa-pencil fa-lg"></span> Submit Comment</Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toogleModal}>
+                    <ModalHeader toggle={this.toogleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                            <FormGroup>
+                                <Label htmlFor="rating">Rating</Label>
+                                <Control.select model=".rating" id="rating" name="rating" className="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="author">Your Name</Label>
+                                <Control.text model=".author" id="author" name="author" placeholder="Your Name" className="form-control" validators={{minLength: minLength(3), maxLength: maxLength(15)}}/>
+                                <Errors
+                                    className="text-danger" model=".author" show="touched" messages={{
+                                        minLength: "Must be greater than 2 characters",
+                                        maxLength: "Must be 15 characters or less"
+                                    }}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="comment">Comment</Label>
+                                <Control.textarea model=".comment" id="comment" name="comment" className="form-control" rows="6" />
+                            </FormGroup>
+                            <Button type="submit" color="primary" >Submit</Button>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </div>
+        )
+    }
+}
+
+    const RenderComment = ({comments}) => {
+        if(comments) {
+            const comment = comments.map( (res) =>  {
+                const date = new Date(res.date)
+                const dateTimeFormat = new Intl.DateTimeFormat('en', { year: 'numeric', month: 'short', day: '2-digit' }) 
+                const [{ value: month },,{ value: day },,{ value: year }] = dateTimeFormat.formatToParts(date)
+                return (
+                    <ul key={res.id} className='list-unstyled'>
+                        <li>{res.comment}</li>
+                        <br/>
+                        <li>--{res.author} , {`${month} ${day}, ${year}`}</li>
+                    </ul>
+                )
+            })
+
+            return (
+                <div className='col-12 col-md-5 m-1'>
+                    <h4>Comments</h4>
+                    {comment}
+                    <CommentForm />
+                </div>
+            )
+        }
+        else {
+            return (<div></div>)
+        }
+    }
 
     const RenderDish = ({dish}) => {
         if(dish) {
@@ -22,34 +119,6 @@ import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardImg, CardText, CardTitl
             return (
                 <div></div>
             )
-        }
-    }
-
-    const RenderComment = ({comments}) => {
-        if(comments) {
-            const comment = comments.map( (res) =>  {
-                console.log(res)
-                const date = new Date(res.date)
-                const dateTimeFormat = new Intl.DateTimeFormat('en', { year: 'numeric', month: 'short', day: '2-digit' }) 
-                const [{ value: month },,{ value: day },,{ value: year }] = dateTimeFormat.formatToParts(date)
-                return (
-                    <ul key={res.id} className='list-unstyled'>
-                        <li>{res.comment}</li>
-                        <br/>
-                        <li>--{res.author} , {`${month} ${day}, ${year}`}</li>
-                    </ul>
-                )
-            })
-
-            return (
-                <div className='col-12 col-md-5 m-1'>
-                    <h4>Comments</h4>
-                    {comment}
-                </div>
-            )
-        }
-        else {
-            return (<div></div>)
         }
     }
 
