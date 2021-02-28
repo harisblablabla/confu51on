6eb3190668a6b777f23cd3ffe0ef66db572972f8
haris fadhilah <haris.fadhilah21@gmail.com>
2021-02-28 20:15:24 +0700

    react animation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { Redirect, Route, Switch, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { postComment, fetchComments, fetchDishes, fetchPromos } from '../redux/action/ActionCreators';
 import { actions } from 'react-redux-form';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 console.log('sebelum dispay')
 
@@ -65,18 +66,23 @@ class Main extends Component {
             postComment={this.props.postComment}/>
           )
         }
+        console.log(this.props.location)
 
         return(
           <div>
             <Header/>
-              <Switch>
-                <Route path="/home" component={HomePage}/>
-                <Route exact path="/aboutus" component={ () => <About leaders={this.props.leaders}/> }/>
-                <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />}/>
-                <Route path="/menu/:dishId" component={DishWithId}/>
-                <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>}/>
-                <Redirect to="/home"/>
-              </Switch>
+            <TransitionGroup>
+              <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+                <Switch key={this.props.location}>
+                  <Route path="/home" component={HomePage}/>
+                  <Route exact path="/aboutus" component={ () => <About leaders={this.props.leaders}/> }/>
+                  <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />}/>
+                  <Route path="/menu/:dishId" component={DishWithId}/>
+                  <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>}/>
+                  <Redirect to="/home"/>
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
             <Footer/>
           </div>
         )
